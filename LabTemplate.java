0c2771fe3ba5d547d662d96900460584736221b4
Scott Opell <me@scottopell.com>
2013-11-12 02:13:34 -0500

    Removes instructions from LabTemplate

--- LabTemplate.java
+++ LabTemplate.java
@@ -11,8 +11,7 @@ public class LabTemplate{
     // But if we want to do something more interesting
     // we can retrieve content from some website here
     // We need to ensure that we use a url that starts with https
-    // the s is important
-    String url = "http://example.com";
+    String url = "https://example.com";
     // We can print out the contents of any https site now!
     System.out.println( enlightenMe(url) );
   }
@@ -21,38 +20,12 @@ public class LabTemplate{
    * Some url's to try
    * https://api.github.com/zen
    *         OR
-   * https://api.github.com/octosay
-   *
-   *
-   * For this part of the lab, you need to choose one of the above urls 
-   * and use it as the 'url' variable in main
-   *
-   * You can compile this file then and run it and see the output.
-   * (Try it multiple times, it changes!)
-   *
-   *
-   * Now check the section "Collaboration" on the lab writeup on github
-   *
-   * Now your partner needs to pick a different url, change it in his copy of your repository,
-   * add, commit and push the changes.
-   * You will then add, commit and attempt to push your changes.
-   *
-   * Git will tell you (in a wordier way) that you haven't pulled in a while and there is new data,
-   * so you will need to pull before you can push your changes.
-   *
-   * When you pull the latest commits, you will be informed that there is a merge conflict.
-   * You need to fix this before you'll be able to push your changes.
-   *
-   * If you recall, a merge conflict occurs when git doesn't know which changes you want.
-   *
-   * So if you open up your copy of LabTemplate.java, you'll see a section that looks
-   *
-   *
+   * https://api.github.com/octocat
    *
    ****************************
   */
 
-  // You can safely ignore this method today
+  // You can safely ignore this method
   public static String enlightenMe(String url){
     try{
       URL zen = new URL(url);
